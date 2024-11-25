72d8cf5e487d4393754d9089ebaacdb5f237261f
Garefild <Garefild2@gmail.com>
2024-11-25 09:21:46 +0200

    wip

---
+++ xbuild.config.ts
@@ -0,0 +1,45 @@
+/**
+ * Import will remove at compile time
+ */
+
+import type { xBuildConfig } from '@remotex-labs/xbuild';
+
+/**
+ * Imports
+ */
+
+import { version } from 'process';
+import pkg from './package.json' with { type: 'json' };
+
+/**
+ * Config build
+ */
+
+const config: Array<xBuildConfig> = [
+    {
+        declaration: true,
+        esbuild: {
+            bundle: true,
+            minify: true,
+            format: 'esm',
+            outdir: 'dist/esm',
+            target: [ `node${ version.slice(1) }` ],
+            platform: 'node',
+            packages: 'external',
+            sourcemap: true,
+            sourceRoot: `https://github.com/remotex-lab/xstruct/tree/v${ pkg.version }/`,
+            entryPoints: [ 'src/index.ts' ]
+        }
+    },
+    {
+        declaration: false,
+        noTypeChecker: true,
+        esbuild: {
+            bundle: true,
+            format: 'cjs',
+            outdir: 'dist/cjs'
+        }
+    }
+];
+
+export default config;
