732ae7abec51d4519d635148f99bb866c731c277
Garefild <Garefild2@gmail.com>
2026-03-21 01:06:28 +0200

    docs: update favicon path in VitePress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,7 +13,7 @@ export default defineVersionedConfig({
     base: '/xStruct/',
     description: 'A binary Serialization Library for TypeScript',
     head: [
-        [ 'link', { rel: 'icon', type: 'image/png', href: '/xStruct/xStruct.png' }],
+        [ 'link', { rel: 'icon', type: 'image/png', href: '/xStruct/logo.png' }],
         [ 'meta', { name: 'theme-color', content: '#ff7e17' }],
         [ 'script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-C30TKES06G' }],
         [
